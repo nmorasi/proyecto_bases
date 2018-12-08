4007a9f8e7023147120ec9b4f2a7a0469bd611a2
nmorasi <nmorasi.369@gmail.com>
2018-12-08 17:07:28 -0600

    agregando lo necesario para poblar cliente , chofer y dueno

--- creacion.sql
+++ creacion.sql
@@ -1,109 +1,82 @@
-CREATE TABLE asociado(
-     id_asociado int,
-     nombre varchar(50),
-     ap_pat varchar(30),
-     ap_mat varchar(30),
-     calle varchar(80), 
-     numero int , 
-     delegacion varchar(80), 
-     fecha_ingreso date, 
-     PRIMARY KEY (id_asociado)
-); 
-CREATE TABLE chofer(
-       id_asociado int, 
-       rfc varchar()
-); 
-CREATE TABLE dueno(
-       id_asociado int,
-       num_licencia varchar()
-      
-);      
-
-CREATE TABLE cliente(
-       id_cliente int,
-       --nombre
-       nombre varchar(50), 
-       ap_pat varchar(25), 
-       ap_mat varchar(25),
-       --direccion
-       calle varchar(80),
-       numero int,
-       delegacion varchar(25),
-       fotografia,
-       telefono , 
-       telefono casa, 
-       lugar_labores ,
-       --informacion opcional 
-       entrada time,
-       salida time , 
-       -- va a decir si es academico estudiante o trabajador para
-       --hacer los descuentos adecuados 
-       tipo char(1), 
-);
-CREATE TABLE correos(
-       id_cliente int,
-       correo varchar(30)
-);
---tambien deberia tener la fecha del viaje 
-CREATE TABLE viaje(
-       id_viaje int,
-       id_asociado int,
-       id_vehiculo int ,
-       distancia real,
-       tiempo time,
-       numero_ocupantes int,
-       varios_destinos char(1)
-);
-
-CREATE TABLE vehiculo(
-       --para saber a quien le pertenece
-       id_asociado int,       
-       numero_economico int,
+DROP TABLE IF EXISTS vehiculo CASCADE;
+DROP TABLE IF EXISTS socio CASCADE;
+DROP TABLE IF EXISTS cliente CASCADE;
+DROP TABLE IF EXISTS socio CASCADE;
+DROP TABLE IF EXISTS agente CASCADE;
+DROP TABLE IF EXISTS chofer CASCADE;
+DROP TABLE IF EXISTS dueno CASCADE;
+CREATE TABLE vehiculo (
+       id_vehiculo int,
        modelo varchar(30),
        marca varchar(30),
-       ano int,
-       num_cilindros int,
+       ano int ,
+       num_cilindros int ,
        num_puertas int,
-       refaccion char(1),
-       est_aut char(1),
-       gas_hib char(1),
-       num_pas int,
+       tipo_transmicion varchar(15),
+       num_pasajeros int,
+       tipo_motor varchar(15),
        cap_tanque real,
-       PRIMARY KEY(numero_economico) 
-       
+       PRIMARY KEY (id_vehiculo)
+) ; 
+
+CREATE TABLE socio (
+       id_socio int,
+       nombre varchar(30),
+       ap_paterno varchar(30),
+       ap_materno varchar(30),
+       calle varchar(80),
+       numero int ,
+       cp int,
+       delegacion varchar(50),
+       f_ingreso date,
+       email varchar(80),
+       tel_movil char(10),
+       foto varchar(100), 
+       PRIMARY KEY(id_socio)
 ); 
-CREATE TABLE ocupantes(
-       id_viaje int ,
-       id_cliente int
+
+CREATE TABLE cliente (
+       id_cliente int,
+       nombre varchar(30),
+       ap_paterno varchar(30),
+       ap_materno varchar(30),
+       delegacion varchar(50),
+       calle varchar(80),
+       numero int,
+       cp int,
+       lugar_adscripcion varchar(150),
+       --a academico , t trabajador , e estudiante 
+       tipo_cliente char(1),
+       hora_e time,
+       hora_s time,
+       foto varchar(100),
+       tel_casa char(8),
+       tel_movil char(10),
+       mail varchar(80),
+       PRIMARY KEY(id_cliente)
 
 );
-
---restricciones a la tabla de los choferes y duenos.
-ALTER TABLE chofer 
-      ADD CONSTRAINT chofer_asociado_fk
-      FOREIGN KEY (id_asociado) references asociado (id_asociado);
+CREATE TABLE dueno(
+       rfc char(14), 
+       id_socio int,
+       PRIMARY KEY (rfc)
+);
+CREATE TABLE chofer(
+       numero_licencia char(8),
+       id_socio int,
+       PRIMARY KEY (numero_licencia)
+);
+CREATE TABLE agente(
+       id_agente int,
+       nombre varchar(30),
+       ap_pat varchar(30),
+       ap_mat varchar(30),
+       PRIMARY KEY (id_agente)
+);
 ALTER TABLE dueno
-      ADD CONSTRAINT dueno_asociado_fk
-      FOREIGN KEY (id_asociado) references asociado (id_asociado);
---restricciones para el viaje
-ALTER TABLE viaje
-      ADD CONSTRAINT viaje_asociado_fk
-      FOREIGN KEY (id_asociado) references asociado (id_asociado),
-      ADD CONSTRAINT viaje_vehiculo_fk
-      FOREIGN KEY (numero_economico) references vehiculo (numero_economico); 
+      ADD CONSTRAINT dueno_socio_fk
+      FOREIGN KEY (id_socio) references socio(id_socio);
       
-      
-
---restricciones para los ocupantes es decir
---que estos pertencezcan a un unico viaje 
-ALTER TABLE ocupantes
-      ADD CONSTRAINT ocupantes_cliente_fk
-      FOREIGN KEY (id_cliente) refeences cliente (id_cliente), 
-      ADD CONSTRAINT ocupantes_viaje_fk
-      FOREIGN KEY (id_viaje) references viaje (id_viaje);
---las restricciones a la tabla del cliente 
-
-ALTER TABLE correo
-      ADD CONSTRAINT correo_cliente_fk
-      FOREIGN KEY (id_cliente) references cliente (id_cliente);
-      
+ALTER TABLE chofer
+      ADD CONSTRAINT chofer_socio_fk
+      FOREIGN KEY (id_socio) references socio(id_socio);
---
+++ poblar.sql
@@ -0,0 +1,5 @@
+\copy vehiculo FROM '/home/nieves/proyecto_bases/proyecto_bases/vehiculo.csv' WITH (DELIMITER ',');
+\copy socio FROM '/home/nieves/proyecto_bases/proyecto_bases/socio.csv' WITH (DELIMITER ',');
+\copy cliente FROM '/home/nieves/proyecto_bases/proyecto_bases/cliente.csv' WITH (DELIMITER ',');
+\copy dueno FROM '/home/nieves/proyecto_bases/proyecto_bases/dueno.csv' WITH (DELIMITER ',');
+\copy chofer FROM '/home/nieves/proyecto_bases/proyecto_bases/chofer.csv' WITH (DELIMITER ',');
