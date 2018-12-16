10470560552604f7aae029b8ff9a06e9463df024
nmorasi <nmorasi.369@gmail.com>
2018-12-16 15:48:52 -0600

    quitando basura

--- consultas_aux.sql
+++
@@ -1,8 +0,0 @@
---dame los que son choferes y duenos de sus autos 
-select count(1)
-from (select id_socio
-          from  socio
-     	  except 
-	  (select count(id_socio)
-            from chofer join socio using (id_socio)
-               join dueno using (id_socio)) ) as A; 
